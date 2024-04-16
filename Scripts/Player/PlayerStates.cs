4eb34729c7445ea068446a0c7ffac33d90fe4250
Harry Clark <139020900+astonHC@users.noreply.github.com>
2024-04-16 23:11:49 +0100

    Update PlayerStates.cs

--- Scripts/Player/PlayerStates.cs
+++ Scripts/Player/PlayerStates.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class PlayerStates : MonoBehaviour
 {
+    public void ON_ENTER_STATE(PlayerStateMachine STATE)
+    {
+    }
+
     public void ON_UPDATE_STATE(PlayerStateMachine STATE)
     {
+    }
+
+    public void ON_EXIT_STATE(PlayerStateMachine STATE)
+    {
+    }
+
+    public void BASE_ENTER_STATE(PlayerStateMachine STATE)
+    {
+        ON_ENTER_STATE(STATE);
     }
 
     public void BASE_UPDATE_STATE(PlayerStateMachine STATE)
     {
         ON_UPDATE_STATE(STATE);
+    }
+
+    public void BASE_EXIT_STATE(PlayerStateMachine STATE)
+    {
+        ON_EXIT_STATE(STATE);
     }
 
     public class StateJump : PlayerStates
